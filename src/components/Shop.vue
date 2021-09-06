17a4089c96de1046bd2040cbb9ce111cf17d2199
alefagi <iltu@libero.it>
2021-09-06 19:38:02 +0200

    fixed shop component with link tags

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -1,36 +1,36 @@
 <template>
   <section id="shop">
     <div class="container">
-      <div>
+      <a href="#">
         <figure>
           <img src="@/assets/img/buy-comics-digital-comics.png" alt="digital-comics">
         </figure>
         <span>digital comics</span>
-      </div>
-      <div>
+      </a>
+      <a href="#">
         <figure>
           <img src="@/assets/img/buy-comics-merchandise.png" alt="merchandise">
         </figure>
         <span>dc merchandise</span>
-      </div>
-      <div>
+      </a>
+      <a href="#">
         <figure>
           <img src="@/assets/img/buy-comics-subscriptions.png" alt="subscriptions">
         </figure>
         <span>subscription</span>
-      </div>
-      <div>
+      </a>
+      <a href="#">
         <figure>
           <img src="@/assets/img/buy-comics-shop-locator.png" alt="shop-locator">
         </figure>
         <span>comic shop locator</span>
-      </div>
-      <div>
+      </a>
+      <a href="#">
         <figure>
           <img src="@/assets/img/buy-dc-power-visa.svg" alt="buy-dc">
         </figure>
         <span>dc power visa</span>
-      </div>
+      </a>
     </div>
   </section>
 </template>
@@ -50,10 +50,11 @@ export default {
       display: flex;
       justify-content: space-evenly;
       align-items: center;
-      div {
+      a {
         height: 100%;
         display: flex;
         align-items: center;
+        text-decoration: none;
         span {
           color: #fff;
           text-transform: uppercase;
