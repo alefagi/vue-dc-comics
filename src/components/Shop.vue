397fb2cdc17316f6f39fecd61a983ac86e735a64
alefagi <iltu@libero.it>
2021-09-06 19:33:19 +0200

    added shop component

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -1,5 +1,38 @@
 <template>
-  <section id="shop"></section>
+  <section id="shop">
+    <div class="container">
+      <div>
+        <figure>
+          <img src="@/assets/img/buy-comics-digital-comics.png" alt="digital-comics">
+        </figure>
+        <span>digital comics</span>
+      </div>
+      <div>
+        <figure>
+          <img src="@/assets/img/buy-comics-merchandise.png" alt="merchandise">
+        </figure>
+        <span>dc merchandise</span>
+      </div>
+      <div>
+        <figure>
+          <img src="@/assets/img/buy-comics-subscriptions.png" alt="subscriptions">
+        </figure>
+        <span>subscription</span>
+      </div>
+      <div>
+        <figure>
+          <img src="@/assets/img/buy-comics-shop-locator.png" alt="shop-locator">
+        </figure>
+        <span>comic shop locator</span>
+      </div>
+      <div>
+        <figure>
+          <img src="@/assets/img/buy-dc-power-visa.svg" alt="buy-dc">
+        </figure>
+        <span>dc power visa</span>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -12,5 +45,27 @@ export default {
   #shop {
     height: 120px;
     background-color: #0282f9;
+    .container {
+      height: 100%;
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+      div {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        span {
+          color: #fff;
+          text-transform: uppercase;
+          font-size: 0.8rem;
+          padding-left: 5px;
+        }
+        figure {
+          height: 50px;
+          width: 50px;
+          display: flex;
+        }
+      }
+    }
   }
 </style>
